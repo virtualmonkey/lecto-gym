4ab26e0afc85c1c24dcaabbf322e65fccae8d61b
virtualmonkey <luis212urbinau@gmail.com>
2023-04-02 09:43:33 -0600

    fix: trying github actions with permissions

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,7 +11,7 @@ const Home = () => {
           Lecto Gym
         </h1>
         <h5 className='home__subtitle'>
-          Mejora tu capacidad lectora y duplica tu rapidez lectora en solo unas semanas mediante divertidos retos interactivos.
+          Mejora tu capacidad lectora y duplica tu rapidez lectora en solo unas semanas mediante divertidos retos interactivos
         </h5>
       </div>
       <Footer />
