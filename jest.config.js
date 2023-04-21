5cdb6979a95665d462b90ccf8751cad55c1661ae
virtualmonkey <luis212urbinau@gmail.com>
2023-04-21 10:51:34 -0600

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./tsconfig");
-
 module.exports = {
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest"
